3702c207159e0cb36502efcd435a7e3c33a49cf2
jb <jessb.epicodus@gmail.com>
2022-01-12 14:14:21 -0800

    remove prompts, add user interface form

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,27 @@
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-
+// bus logic
 function add(number1, number2) {
   return number1 + number2;
 }
 
-function subtract(num1,num2){
-  return(num1-num2);
+function subtract(number1,number2){
+  return number1 - number2;
 }
-const subResult= subtract(number1,number2)
 
 function mult(number1, number2){
   return number1 * number2;
 }
 
-function divide(num1,num2){
-  return num1/num2;
+function divide(number1,number2){
+  return number1/number2;
 }
-alert(divide(number1,number2));
+
+//user interface logic
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
